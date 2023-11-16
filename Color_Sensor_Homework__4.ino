c8f6a385b430e41b7ebde64c28c0419d522e0c92
DLRDLR38 <149529703+DLRDLR38@users.noreply.github.com>
2023-11-16 10:15:14 -0600

    Update Color_Sensor_Homework__4.ino

--- Color_Sensor_Homework__4.ino
+++ Color_Sensor_Homework__4.ino
@@ -74,14 +74,10 @@ while(1)
 {
 //save value read from the frequencyOut pin of the TCS3200 color sensor into the variable colorTested
 colorIdentified = digitalRead(frequencyOut);
-
-}
-
-//if statement to compare value read from frequencyOut pin of TCS3200 and compare it to the known value of blueLight.
-//To allow for small discrepancies the value of 10 has been added/subtracted to blueLight
+//if statement to compare value read from frequencyOut pin of TCS3200 and compare it to the known value of blueLight. To allow for small discrepancies the value of 10 has been added/subtracted to blueLight
 if (colorIdentified > constrain(blueLight, blueLight - 10, blueLight +10) || colorIdentified < constrain(blueLight,blueLight - 10, blueLight +10))
 {
 Serial.println("A blue object has been detected for the robotic arm to pick up");
 }
-
+}
 }
